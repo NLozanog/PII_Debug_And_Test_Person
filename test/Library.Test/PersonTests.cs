8ad9d374c668d28b8e4f077b90a7138fafc27cdc
NLozanog <42289342+NLozanog@users.noreply.github.com>
2022-04-23 11:58:58 -0300

    Update PersonTests.cs
    Se agregaron los test correspondientes

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -6,16 +6,51 @@ namespace Tests
 {
     public class Tests
     {
+        private Person Nicolas;
         [SetUp]
         public void Setup()
         {
-            // Insertá tu código de inicialización aquí
+            Nicolas = new Person("Nicolás Lozano","5.554.976-8");
         }
-
+        /*
+        Test que verifica que no se cambie la cedula si esta es invalida
+        */
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void Test_cedulaInvalida() 
         {
-            // Insertá tu código  de pruebaaquí
+            string Expected_ID = Nicolas.ID;
+            Nicolas.ID = "5.554.456-1";
+            Assert.AreEqual(Nicolas.ID,Expected_ID);
+        }
+        /*
+        Test que verifica que cambie la cedula si es valida
+        */
+        [Test]
+        public void Test_cedulaValida() 
+        {
+            string Expected_ID = "5.554.975-2";
+            Nicolas.ID = Expected_ID;
+            Assert.AreEqual(Nicolas.ID,Expected_ID);
+        }   
+        /*
+        Test que verifica que cambie el nombre si es valido
+        */
+        [Test]
+        public void Test_nombreValido() 
+        {
+            string Expected_Name = "Frodo Bolsón";
+            Nicolas.Name = Expected_Name;
+            Assert.AreEqual(Nicolas.Name,Expected_Name);
+        }
+        /*
+        Test que verifica que no se cambie el nombre si es vacío
+        */
+        [Test]
+        public void Test_nombreVacio() 
+        {
+            string Expected_Name = Nicolas.Name;
+            Nicolas.Name = "";
+            Assert.AreEqual(Nicolas.Name,Expected_Name);
         }
     }
 }
